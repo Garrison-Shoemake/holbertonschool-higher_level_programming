099ad05e1344bde97b0e32d10809befc1944cbd1
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-04-27 21:08:48 +0000

    add task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(0, len(c)):
-        if ord(i) >= 97 and <= 122:
+        if ord(chr(i)) >= 97 and ord(chr(i)) <= 122:
             return True
         else:
             return False
