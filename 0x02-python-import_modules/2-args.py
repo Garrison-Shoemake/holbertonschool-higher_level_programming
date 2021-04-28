bb800d4959b4a1206697b6fad280e03bb1dc1b8a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-04-28 04:40:07 +0000

    add 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,11 @@
 import sys
 args = len(sys.argv)
 if __name__ == "__main__":
-    for i in range(0, args):
-        if i == 0:
-            print("0 arguments.")
-        elif args > 0:
-            if args == 1:
-                print("1 argument:")
-                print("{}: {}".format(i, sys.argv[i]))
-            else:
-                print("{} arguments:".format(args))
-                print("{}: {}".format(i, sys.argv[i]))
+    if args == 1:
+        print("0 arguments.")
+    elif args == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args - 1))
+    for i in range(1, args):
+        print("{}: {}".format(i, sys.argv[i]))
