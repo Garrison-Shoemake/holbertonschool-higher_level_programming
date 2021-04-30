00091af43ee57836674364cc66b0e585b1786f84
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-04-30 18:50:03 +0000

    add 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    a_dictionary.update((x, y*2) for x, y in a_dictionary.items())
-    return a_dictionary
+    newdict = a_dictionary.copy()
+    newdict.update((x, y*2) for x, y in a_dictionary.items())
+    return newdict
