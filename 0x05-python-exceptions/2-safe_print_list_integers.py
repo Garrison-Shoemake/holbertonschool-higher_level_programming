bf1a3dea5bf494b615a4d964f590b5e11672e01a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-04 15:35:11 +0000

    add pepe check

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             tf = isinstance(my_list[i], int)
-            if tf == True:
+            if tf is True:
                 print("{}".format(my_list[i]), end="")
                 ii += 1
         except (ValueError, TypeError) as Error:
