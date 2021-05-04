97784fbfb4ec65b8a1f7a439a04ad4a8f965c451
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-04 03:50:44 +0000

    add task 0 after learning about range

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for x in my_list:
-                print("{}".format(my_list[x]), end="")
-    except:
-        pass
+    ii = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ii += 1
+        except:
+            pass
+
     print()
-    return x
+    return ii
