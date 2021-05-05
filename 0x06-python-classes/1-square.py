60d0efe645465b5158c5b3e6208d6486e18862fe
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-05 01:39:45 +0000

    add 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" No longer blank, now has size"""
+
+
+class Square():
+    """ Square's only attribut is size atm """
+
+    def __init__(self, size):
+        """ Initialization method, variables go here """
+
+        self._size = size
+
+    pass
