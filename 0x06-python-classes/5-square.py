bbedaf04cc7fadf49d6627df69b8caa4f4cdeab9
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-05 02:54:34 +0000

    add 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Square will now have a check on size"""
+
+
+class Square():
+    """ Square's only attribute is size atm """
+
+    def __init__(self, size=0):
+        """ Initialization method, variables go here
+        In addition, this checks that size is 0 or more """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size**2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print()
