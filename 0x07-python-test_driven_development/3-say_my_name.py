2ae517f096559befbdeb67bb9bba15897901ce30
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-09 23:49:41 +0000

    add task 2 and test file

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This function prints a name! """
+
+
+def say_my_name(first_name, last_name=""):
+    """ This is the function that prints a name, after checking that it is a string """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
