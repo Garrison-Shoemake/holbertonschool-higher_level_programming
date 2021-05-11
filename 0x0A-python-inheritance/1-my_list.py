07cddff09930b837ac3dc6bcc0391ca55f713d5c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-11 23:32:53 +0000

    add 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This Class will inherit from list """
+
+
+class MyList(list):
+    """ MyList will print a list, sorted in ascending order """
+
+
+    def print_sorted(self):
+        print(sorted(self))
