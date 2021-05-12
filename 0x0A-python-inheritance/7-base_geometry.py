fe60dfd77eb0d3d037a1a64d090adb3613a930c9
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-12 00:41:32 +0000

    add 7, but no tests

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" New Class named BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry, class that raises and exception for area and
+    validates a value given in integer_validator """
+
+    def area(self):
+        """ currently empty, only raises exception """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance (value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
