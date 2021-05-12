32b5daa20415e1c35e81095b86480e5ab6623d5b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-12 00:33:30 +0000

    add 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" New Class named BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry, class that only raises and exception """
+
+    def area(self):
+        """ currently empty, only raises exception """
+
+        raise Exception("area() is not implemented")
