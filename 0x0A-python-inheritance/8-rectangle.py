160c6d2f88e44faa3e30f7f968a90fa0526e6f7f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-12 01:13:13 +0000

    add 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" New Class named BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry, class that raises and exception for area and
+    validates a value given in integer_validator """
+
+    def area(self):
+        """ currently empty, only raises exception """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance (value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """ Subclass of BaseGeometry """
+
+    def __init__(self, width, height):
+        if super().integer_validator("width", width):
+            self.width = width
+        if super().integer_validator("height", height):
+            self.height = height
