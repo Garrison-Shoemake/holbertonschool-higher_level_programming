0a77a07440b37fc329e8683d03113f96e71dd386
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-12 22:16:42 +0000

    add 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This function writes a string to a text file and returns character count """
+
+
+def write_file(filename="", text=""):
+    """ this function calls with and print with a counter variable
+    return the counter variable, function should creat the file
+    if it doesnt exist and overwrites the content if it exists """
+
+    with open(filename, 'w') as f:
+        f.write(text)
