d6c09eec957427d602a5b0763aee1a7ced1a04ba
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-12 00:07:10 +0000

    add 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" this function checks for inheritance as well """
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns isinstance result """
+
+    return isinstance(obj, a_class)
