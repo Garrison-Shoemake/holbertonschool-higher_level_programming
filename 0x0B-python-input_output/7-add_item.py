dc6e18660ad2b43b952dd0ff9b9a1a57c251cf4c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-13 04:32:24 +0000

    add 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This file loads, adds, and saves files """
+
+
 import sys
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_json_file = __import__('6-load_from_json_file.py').load_from_json_file
