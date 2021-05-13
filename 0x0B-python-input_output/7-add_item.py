96b60bb291cf745c389142b23f4884b56b62b50d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-13 04:31:15 +0000

    add 7 docstring

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-""" this script will add arguments to a list and save them to a file """
-
-
+""" This file loads, adds, and saves files """
 import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
