598d9a36a60b5bd9a318189c3543ddf90adf0d21
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-13 04:25:03 +0000

    add 7 documentation?

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,15 @@
 
 import sys
 
+
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+load_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
 
 try:
-    lst = load_from_json_file('add_item.json')
+    lst = load_json_file('add_item.json')
 except:
     lst = []
 
-lst +=  argv[1:]
+lst += argv[1:]
 save_to_json_file(lst, 'add_item.json')
