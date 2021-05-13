0551b704ba00389add370854fd1624e2a9421155
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-13 04:01:39 +0000

    add 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this file includes a class with definitions """
+
+
+class Student():
+    """ This class covers students, with parameters: first
+    and last name and age. """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        atts = {}
+
+        if attrs is not None:
+            for i in attrs:
+                if i in self.__dict__:
+                    atts[i] = self.__dict__[i]
+            return atts
+        else:
+            return vars(self)
+
+    def reload_from_json(self, json):
+        for i in json:
+            setattr(self, i, json[i])
