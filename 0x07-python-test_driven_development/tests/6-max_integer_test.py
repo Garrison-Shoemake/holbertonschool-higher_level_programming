51b906dbbf7b1dad1f63e1b4df320ab21c39eac5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-13 19:40:49 +0000

    add 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,6 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_these(self):
         self.assertEqual(max_integer([1,2,3,4,5]), 5)
-        self.assertIsInstance(max_integer([1, 2, 3, 4, 5]), list)
         self.assertRaises(TypeError, max_integer((1,2)))
         self.assertRaises(TypeError, max_integer([0.123, 3.14159]))
         self.assertRaises(TypeError, max_integer("string"))
