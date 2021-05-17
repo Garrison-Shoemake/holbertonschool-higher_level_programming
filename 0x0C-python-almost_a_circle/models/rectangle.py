9d75e0c3e787652c25d5223a944fe4890a745260
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-17 01:04:57 +0000

    add str override

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,10 @@ class Rectangle(Base):
             for i in range(self.__width):
                 print('#', end="")
             print()
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x,
+                                                       self.y,
+                                                       self.width,
+                                                       self.height)
