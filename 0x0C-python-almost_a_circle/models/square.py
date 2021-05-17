743739afd01ed906ad6e7530f9283cd21951f945
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-17 21:45:48 +0000

    tinkering with documentation... again

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,7 +35,6 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """ this updates similarly to rectangle """
-
         ids = ['id', 'size', 'x', 'y']
         i = 0
         if args:
