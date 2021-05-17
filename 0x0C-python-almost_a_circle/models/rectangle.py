f16350195ce05e9f58c20ca2d37d0e53b5a26c60
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-17 00:29:28 +0000

    deprivatized variables in init

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,14 +7,14 @@ from models.base import Base
 
 class Rectangle(Base):
     """ This is the Rectangle subclass, derived from Base
-    should also have unique ids, as per Base's instantiation """
+    should also have unique ids, as per Base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -52,14 +52,14 @@ class Rectangle(Base):
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be > 0")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value <= 0:
-            raise ValueError("y must be > 0")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
