64755900bcbc70e8e54e5278789a079b89008737
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-17 22:49:45 +0000

    add new base method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ This is our Base Class file """
 
 
+import json
+
+
 class Base:
     """ Base Class information goes here """
 
@@ -13,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
