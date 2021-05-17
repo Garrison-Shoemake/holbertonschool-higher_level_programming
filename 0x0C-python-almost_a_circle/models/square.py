aee8bd007f0e3bfbb0927806bfe66c412eadf25d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-17 20:46:19 +0000

    add square.py, checking checker

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This is the Square class, derived from Rectangle """
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ This Class is a Square class, reminding us that a
+    square is just a rectangle with equal sides """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, size, size, x, y)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.__width = size
+        self.__height = size
+
+    def __str__(self)
+    """ string override """
+    return "[Square] ({}) {}/{} - {}".format(self.id,
+                                             self.x,
+                                             self.y,
+                                             self.size)
