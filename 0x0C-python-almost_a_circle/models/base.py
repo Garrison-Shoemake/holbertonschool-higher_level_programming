2dd98390b680fb7eda4365e3dd462c5e7a8cf3f2
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-18 00:21:25 +0000

    add dummy creation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,9 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = Square(2)
+        dummy.update(**dictionary)
+        return dummy
