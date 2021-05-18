14a2d8e6b8f4a490dd97903392a9b586ef1c236f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-18 04:22:29 +0000

    add square tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" this is the unittest file for the Base class """
+
+
+import unittest
+from models.square import Square
+
+
+class SqrTest(unittest.TestCase):
+    """ These are the unit tests for the base class """
+
+    def test_negatives(self):
+        """ this method tests for negative numbers in Rectangle """
+
+        with self.assertRaises(ValueError):
+            b1 = Square(-1, 2, 3)
+        with self.assertRaises(ValueError):
+            b2 = Square(1, -2, 3)
+        with self.assertRaises(ValueError):
+            b3 = Square(1, 2, -3)
+
+    def test_strings(self):
+        """ this method tests for strings in Rectangle """
+
+        with self.assertRaises(TypeError):
+            b1 = Square("1", 2, 3)
+        with self.assertRaises(TypeError):
+            b2 = Square(1, "2", 3)
+        with self.assertRaises(TypeError):
+            b3 = Square(1, 2, "3")
+
+    def test_zero(self):
+        """ this method tests if width and height are 0 """
+
+        with self.assertRaises(ValueError):
+            b2 = Square(0, 2)
+
+    def test_update(self):
+        s = Square(1, 0, 0, 1)
+        self.assertEqual(str(s), "[Square] (1) 0/0 - 1")
+        s.update(1, 2)
+        self.assertEqual(str(s), "[Square] (1) 0/0 - 2")
+        s.update(1, 2, 3)
+        self.assertEqual(str(s), "[Square] (1) 3/0 - 2")
+        s.update(1, 2, 3, 4)
+        self.assertEqual(str(s), "[Square] (1) 3/4 - 2")
+
+if __name__ == '__main__':
+    unittest.main()
