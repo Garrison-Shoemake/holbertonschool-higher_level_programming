300d3c19be67de9cf0e86f8657c9a48b3a3c74ba
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-26 22:12:22 +0000

    add 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- this script will create a table in the current database
 -- using CREATE, with special flags as per task
 CREATE TABLE IF NOT EXISTS first_table (
-id INT
-name VARCHAR(256)
+id INT,
+name VARCHAR(256),
 );
