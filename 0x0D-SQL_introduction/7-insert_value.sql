40edce08c62b48a566c55db824f04d6a2ea507cd
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-26 22:44:21 +0000

    add 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
 -- this script inserts a row in a table with new information
-INSERT INTO first_table(
-id, name) VALUES (89, Holberton School)
-);
+INSERT INTO first_table (id, name) VALUES (89, Holberton School);
