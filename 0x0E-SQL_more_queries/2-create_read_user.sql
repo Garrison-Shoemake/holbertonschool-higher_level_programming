1329adb6bc4083a9d7e16940b337ce5fc464ab4f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-27 20:17:43 +0000

    add 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- this script creates a database and adds a user
 -- user will have only select privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT PRIVILEGE ON 'hbtn_0d_2'@'localhost'.* TO 'user_0d_2'@'localhost';
