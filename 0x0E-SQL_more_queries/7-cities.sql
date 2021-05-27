376e9e5509535f747e039a0fb0e6cdddf0e3482a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-27 23:24:03 +0000

    add 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- script that creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY,
-state_id INT NOT NULL FOREIGN KEY(id) REFERENCES hbtn_0d_usa.states(id),
+id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+state_id INT NOT NULL FOREIGN KEY(id) REFERENCES states(id),
 name VARCHAR(256) NOT NULL
 );
