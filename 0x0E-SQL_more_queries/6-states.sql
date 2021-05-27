98ab86424473d91717f6a44ff0398a8e9f13f1ca
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-27 22:34:00 +0000

    add 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- creates database and table with specific parameters for id and name
 CREATE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-id INT UNIQUE NOT NULL AUTO_INCREMENT,
+id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256) NOT NULL,
-PRIMARY KEY (ID)
 );
