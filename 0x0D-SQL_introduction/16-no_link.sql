fb38dd9fcf89d14137ddd43b7ab45e99a9aa5daa
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-27 04:12:28 +0000

    add 16 edits

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- lists all records of a table
 -- results should display score and name
 -- records should be listed by DESC
-SELECT score name FROM second_table WHERE name IS NOT NULL ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name is not null ORDER BY DESC;
