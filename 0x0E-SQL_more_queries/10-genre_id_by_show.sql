b1363fdb00136afab57d4c6cb8d068184aa24b21
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-28 02:29:17 +0000

    add 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 -- ordered in ASC by tv title and genre id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genre
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
