f8efbed1846db4587d77bc3147712401e1eba059
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-28 01:53:14 +0000

    forgot s on states and INNER

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- this script lists starts use of JOIN
-SELECT cities.id, cities.name, state.name
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states
+INNER JOIN states
 ON cities.state_id = state.id;
