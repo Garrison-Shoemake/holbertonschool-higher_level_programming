a7641e9697495139ce92a34716387a57a3a8befb
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-28 02:32:54 +0000

    i meant left

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- same as 10, but LEFT JOIN
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-RIGHT JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
