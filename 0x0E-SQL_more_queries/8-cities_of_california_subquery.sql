00150ce44768cbde68c534806c8d54db8d14a32d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-28 00:51:17 +0000

    add 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- this script lists cities found in a database
+-- stats table contains on one record: name = California
+-- sort results in ascending order by cities.id
+-- no JOIN
+SELECT *
+FROM hbtn_0d_usa.states
+WHERE state_id.cities = 1,
+ORDER BY cities.id ASC
