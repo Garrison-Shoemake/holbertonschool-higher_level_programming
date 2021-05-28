4e72fb6264887614595600152fbd4a144a8203b1
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-05-28 01:16:25 +0000

    add 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,10 +2,10 @@
 -- stats table contains on one record: name = California
 -- sort results in ascending order by cities.id
 -- no JOIN
-SELECT *
+SELECT id, name
 FROM hbtn_0d_usa.cities
 WHERE state_id.cities =
- (SELECT *
+ (SELECT id
   FROM hbtn_0d_usa.states
   WHERE name = california),
 ORDER BY cities.id ASC;
