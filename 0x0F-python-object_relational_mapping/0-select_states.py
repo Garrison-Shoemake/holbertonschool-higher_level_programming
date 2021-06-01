91a21cfeef5e02444a52aa64270d6b1bf9ad1d5b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-01 19:29:35 +0000

    pep8 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ this script lists states from a database """
 
-
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-
 
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
     cursor = db.cursor()
