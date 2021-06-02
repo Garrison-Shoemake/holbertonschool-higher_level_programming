d628cd7f531c8d1a96e09aa4c5a314862ea3d834
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-02 00:57:10 +0000

    add model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ this is a state class that inherits from SQLAlchemy """
 
 from sqlalchemy import Column, Integer, String
@@ -6,8 +7,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+
     """ model class from SQLAlchemy """
+
     __tablename__ = states
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
