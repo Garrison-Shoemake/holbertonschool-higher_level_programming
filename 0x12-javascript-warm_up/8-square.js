c0567e791a53a843c78e145d8ffba414d4318be5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-03 03:38:28 +0000

    add 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+let i = 0;
+const args = process.argv;
+const newarg = parseInt(args[2], 10);
+
+if (isNaN(newarg) === true) {
+  console.log('Missing size');
+} else {
+  while (i <= newarg) {
+    console.log('X'.repeat(newarg));
+    i++;
+  }
+}
